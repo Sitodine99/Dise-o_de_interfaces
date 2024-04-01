f93955b052c1e2e08624f8d676a37a0b0e302f11
Sitodine99 <josamontero@gmail.com>
2024-04-01 17:46:47 +0200

    Añado nuevo Readme y estructura de archivos

---
+++ Ejercicios/06MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ejercicio_06
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static void rellenarComboBox(ComboBox combo)
+        {
+            for (int f = 0; f <= 225; f++)
+            {
+                combo.Items.Add(f.ToString());
+            }
+        }
+
+        private void btnPinta_Click(object sender, RoutedEventArgs e)
+        {
+            byte r = (byte)comboRojo.SelectedIndex;
+            byte g = (byte)comboVerde.SelectedIndex;
+            byte b = (byte)comboAzul.SelectedIndex;
+
+            Color color = Color.FromRgb(r, g, b);
+            SolidColorBrush brush = new SolidColorBrush(color);
+            btnPinta.Background = brush;
+        }
+
+        public MainWindow()
+
+        {
+            InitializeComponent();
+            rellenarComboBox(comboRojo);
+            rellenarComboBox(comboAzul);
+            rellenarComboBox(comboVerde);
+        }
+    }
+}
---
+++ Ejercicios/README.md
@@ -0,0 +1,26 @@
+# Ejercicios de Diseño de Interfaces - JAVA
+
+
+[Ejercicios prouestos en clase 01 y 02](Propuesta_de_ejercicio_01_02.java) - Crea un JFrame con las siguientes características:
+
+    - Posición (x,y): (10,20)
+    - Tamaño: 1000x700px
+    - Título de la ventana: M07 Videotutoria 2 - UF1
+    - Color de fondo #99CCFF
+    - Cerrar al salir
+
+El frame anterior tiene que tener al menos:
+
+    - Etiqueta
+    - Campo de texto
+    - Botón
+    - El usuario introduce un "Nombre" y al pulsar ok, se muestra una ventana con el saludo.
+
+[Ejercicios propuestos en clase 03 y 03.1](Propuesta_de_ejercicio_03_03.1.java) - Crea un formulario y añádele funcionalidad al botón aceptar; para que después de rellenar los datos, al pulsar Aceptar, de la bienvenida al usuario.
+
+Ejercicio propuesto en clase 04 y 05 - Crea la interfaz de una calculadora en una ventana de 300x300. Añade la funcionalidad Button_Click y programa la calculadora - [xaml](MainWindow.xaml) - [xaml.cs](MainWindow.xaml.cs)
+
+Ejercicio propuesto en clase 06 - Crea la interfaz para elegir valores de rojo, verde y azul (RGB) y cambiar el color del botón con el color elegido, en una ventana de 300x400 - [xaml](06MainWindow.xaml) - [xaml.cs](MainWindow.xaml.cs)
+
+[Ejercicio propuesto en clase 07](ComboRGB.java) - Usando NetBeans; a partir de los valores RGB seleccionados, configurar el color de fondo del botón "Pintar botón".
+
